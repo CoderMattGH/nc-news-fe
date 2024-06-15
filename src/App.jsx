309e9e49ca5e64ffe05b55f2236767a1b4c6bec3
GANDHIX <mattdixon2000@hotmail.com>
2024-06-15 21:28:15 +0100

    Added Articles into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import Articles from './components/Articles';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main-body">
+        <Articles />
+      </main>
     </>
   )
 }
