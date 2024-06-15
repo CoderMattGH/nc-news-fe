72351ae3f0b625eca35dee7c2813985776abc4ef
GANDHIX <mattdixon2000@hotmail.com>
2024-06-15 21:27:47 +0100

    Added Articles component

---
+++ src/components/Articles.jsx
@@ -0,0 +1,11 @@
+import './Articles.css';
+
+function Articles() {
+  return (
+    <section className="articles-section">
+      <h1>TESTING</h1>
+    </section>
+  );
+}
+
+export default Articles;
