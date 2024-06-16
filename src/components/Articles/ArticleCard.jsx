ff3a818c7a8367a30df24922fd960cc7f5ca2c89
GANDHIX <mattdixon2000@hotmail.com>
2024-06-16 16:32:43 +0100

    Added ArticleCard component

---
+++ src/components/Articles/ArticleCard.jsx
@@ -0,0 +1,34 @@
+import dateParsing from '../../util-functions/date-parsing';
+
+// TODO :Author
+function ArticleCard({article}) {
+  return (
+    <>
+      <img className="article-card__img" src={article.article_img_url} />
+      <h2 className="article-card__title">{article.title}</h2>
+      <p className="article-card__desc-summary">
+        {(article.body_preview.length >= 380) ?
+            article.body_preview.slice(0, 380) + "..."
+          : 
+            article.body_preview
+        }
+      </p>
+      <div className="article-card__footer">
+        <p className="article-card__votes article-card__element--gray">
+          <img className="article-card__vote_btn" src="./images/buttons/upvote.svg" />
+          <span className="article-card__vote_cnt">{article.votes}</span>
+          <img className="article-card__vote_btn" src="./images/buttons/downvote.svg" />
+        </p>
+        <p className="article-card__category article-card__element--gray">{article.topic}</p>
+        <p className="article_card__date article-card__element--gray">
+          {dateParsing.convertUnixDate(article.created_at)}
+        </p>
+        <p className="article_card__comment-count article-card__element--gray">
+          {article.comment_count} comments
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default ArticleCard;
