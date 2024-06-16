47a20b9b4a1ce5318d6f5b4d4598e858c0dc6e58
GANDHIX <mattdixon2000@hotmail.com>
2024-06-16 01:46:59 +0100

    Added Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import './Loading.css';
+
+function Loading() {
+  return (
+    <section className="loading-section">
+      {/* <p>Loading...</p> */}
+      <img className="loading-icon" src="./images/loading_icon.svg"></img>
+    </section>
+  );
+}
+
+export default Loading;
