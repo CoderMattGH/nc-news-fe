e2072b63c23b922288d00e9e0450fb5612d3c9a0
GANDHIX <mattdixon2000@hotmail.com>
2024-06-17 21:47:53 +0100

    Added Article route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import {Routes, Route} from 'react-router-dom';
 
 import Header from './Header';
 import Articles from './Articles';
+import Article from './Article';
 
 import './App.css';
-
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Articles />} />
           <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:article_id" element={<Article />} />
         </Routes>
       </main>
     </>
