ff2140f0a384beb9087b20c6be76e27e3b604f9f
GANDHIX <mattdixon2000@hotmail.com>
2024-06-17 22:30:31 +0100

    Added Article API endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 const BASE_API_URL = `https://be-nc-news-4pai.onrender.com/api`;
 const ARTICLES_API_URL = `${BASE_API_URL}/articles`;
+const ARTICLE_BASE_API_URL = `${BASE_API_URL}/articles/`;
 
-export default {BASE_API_URL, ARTICLES_API_URL};
+export default {BASE_API_URL, ARTICLES_API_URL, ARTICLE_BASE_API_URL};
