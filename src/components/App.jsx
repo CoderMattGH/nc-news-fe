e84b5fcf3fdcbbe32371365f7d7ebce9da224277
GANDHIX <mattdixon2000@hotmail.com>
2024-06-17 21:38:00 +0100

    Moved App component into component folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 
-import Header from './components/Header';
-import Articles from './components/Articles';
+import Header from './Header';
+import Articles from './Articles';
 
 import './App.css';
 
