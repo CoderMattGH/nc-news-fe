378fe2ce4adc6e78aaa3f570bb96cb65c1c5591f
GANDHIX <mattdixon2000@hotmail.com>
2024-06-17 23:14:52 +0100

    Abstracted some styles to globl style file

--- src/components/Articles/ArticleCard.jsx
+++ src/components/Articles/ArticleCard.jsx
@@ -16,16 +16,16 @@ function ArticleCard({article}) {
         }
       </p>
       <div className="article-card__footer">
-        <p className="article-card__votes article-card__element--gray">
-          <img alt="upvote" className="article-card__vote_btn" src="./images/buttons/upvote.svg" />
-          <span className="article-card__vote_cnt">{article.votes}</span>
-          <img alt="downvote" className="article-card__vote_btn" src="./images/buttons/downvote.svg" />
+        <p className="button__votes button__element--gray">
+          <img alt="upvote" className="button__vote_btn" src="/images/buttons/upvote.svg" />
+          <span>{article.votes}</span>
+          <img alt="downvote" className="button__vote_btn" src="/images/buttons/downvote.svg" />
         </p>
-        <p className="article-card__category article-card__element--gray">{article.topic}</p>
-        <p className="article_card__date article-card__element--gray">
+        <p className="button__category button__element--gray">{article.topic}</p>
+        <p className="article_card__date button__element--gray">
           {dateParsing.convertUnixDate(article.created_at)}
         </p>
-        <p className="article_card__comment-count article-card__element--gray">
+        <p className="article_card__comment-count button__element--gray">
           {article.comment_count} comments
         </p>
       </div>
