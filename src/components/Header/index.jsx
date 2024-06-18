c7a78209d52a961f3a0d7c4c46623ad7305ce1b2
GANDHIX <mattdixon2000@hotmail.com>
2024-06-18 09:57:17 +0100

    Added link to home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import MenuBar from './MenuBar';
 import SearchBar from './SearchBar';
 
@@ -7,8 +8,12 @@ function Header() {
   return (
     <header className="header">
       <div className="title">
-        <img alt="Title Logo" className="title__logo" src="/images/logo.svg" />
-        <img alt="Title Image" className="title__img" src="/images/title_main.svg" />
+        <Link className="link" to="/">
+          <img alt="Title Logo" className="title__logo" src="/images/logo.svg"/>
+        </Link>
+        <Link className="link" to="/">
+          <img alt="Title Image" className="title__img" src="/images/title_main.svg" />
+        </Link>
       </div>
       <SearchBar />
       <MenuBar />
