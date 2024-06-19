c5d0c6b9f8a51fa3568a3b802b4e0bbf48218741
GANDHIX <mattdixon2000@hotmail.com>
2024-06-19 18:52:59 +0100

    Added buttson for Login/Login/Register

--- src/components/Header/MenuBar.jsx
+++ src/components/Header/MenuBar.jsx
@@ -1,4 +1,5 @@
 import {useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 import {UserContext} from '../../contexts/User';
 
@@ -7,19 +8,42 @@ import './MenuBar.css';
 function MenuBar() {
   const {user} = useContext(UserContext);
 
+  const navigate = useNavigate();
+
+  const clickLogin = (event) => {
+    event.preventDefault();
+
+    navigate("/login");
+  };
+
+  const clickRegister = (event) => {
+    event.preventDefault();
+
+    navigate("/register");
+  };
+
+  const clickLogout = (event) => {
+    event.preventDefault();
+
+    navigate("/logout");
+  };
+
   return (
     <nav className="nav">
       {(user) ?
           <li className="nav__entry">
-            Logout
+            <button className="menu-bar-button menu-bar-button__logout" onClick={clickLogout}>
+              Logout:
+              <span className="menu-bar-button__username">{user.username}</span>
+            </button>
           </li>
         :
           <>
             <li className="nav__entry">
-              Log in
+              <button className="menu-bar-button" onClick={clickLogin}>Log in</button>
             </li>
             <li className="nav__entry">
-              Register
+              <button className="menu-bar-button" onClick={clickRegister}>Register</button>
             </li>
           </>
       }
