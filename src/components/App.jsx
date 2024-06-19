1b9131b334834ee7610b2eee0a03576ac0940e8f
GANDHIX <mattdixon2000@hotmail.com>
2024-06-20 00:55:27 +0100

    Added initial ability to track user votes to prevent revoting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {useContext, useEffect, useState} from 'react';
+import {useContext, useEffect, useRef, useState} from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import Header from './Header';
@@ -20,6 +20,10 @@ function App() {
   // If errOverlayMsg is null, then no error overlay is displayed.
   const [errOverlayMsg, setErrOverlayMsg] = useState(null);
 
+  // Keep track of which articles the user has voted on.
+  // Example object: {article_id: 1, increment: -1}
+  const userVotes = useRef([]);
+
   const {user} = useContext(UserContext);
 
   useEffect(() => {
@@ -27,12 +31,24 @@ function App() {
     new Image().src = '/images/loading_icon.svg';
   }, []);
 
+  // On user context change, empty votes array.
+  useEffect(() => {
+    console.log("Resetting user votes!");
+
+    userVotes.current = [];
+  }, [user]);
+
   const upDownVoteArticle = async (articleId, increment) => {
     if (!user) {
       setErrOverlayMsg(constants.ERR_MSG_NOT_LOGGED_IN);
 
       throw new Error("USER_NOT_LOGGED_IN");
     }
+
+    if (!canUserVote(articleId, increment))
+      throw new Error("USER_ALREADY_VOTED");
+
+    addToUserVotes(articleId, increment);
 
     // Try and upvote article
     const url = `${constants.ARTICLE_BASE_API_URL}${articleId}`;
@@ -50,6 +66,38 @@ function App() {
 
           throw new Error("SERVER_ERROR");
         });
+  };
+
+  const addToUserVotes = (articleId, increment) => {
+    // Search current array for existing object
+    for (const userObj of userVotes.current) {
+      if (userObj.article_id === articleId) {
+        userObj.increment += increment;
+
+        return;
+      }
+    }
+
+    const voteObj = {article_id: articleId, increment: increment};
+
+    userVotes.current = [...userVotes.current, voteObj];
+  };
+
+  const canUserVote = (articleId, increment) => {
+    for (let i = 0; i < userVotes.current.length; i++) {
+      const voteObj = userVotes.current[i];
+
+      if (voteObj.article_id === articleId) {
+        if ((voteObj.increment + increment > 1) || 
+            (voteObj.increment + increment < -1)) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+
+    return true;
   };
 
   return (
