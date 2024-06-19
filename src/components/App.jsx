db232edfc4a80c41d12b39ce7efe9a080594ca3c
GANDHIX <mattdixon2000@hotmail.com>
2024-06-19 22:01:26 +0100

    Added ErrorOverlay component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import Header from './Header';
@@ -7,37 +7,41 @@ import Article from './Article';
 import Login from './Login';
 import Logout from './Logout';
 import Register from './Register';
+import ErrorOverlay from './ErrorOverlay';
 
 import {UserContext} from '../contexts/User';
+
+import constants from '../constants';
 
 import './App.css';
 
 function App() {
+  // If errOverlayMsg is null, then no error overlay is displayed.
+  const [errOverlayMsg, setErrOverlayMsg] = useState(null);
+
+  const {user} = useContext(UserContext);
+
   useEffect(() => {
     console.log("Caching loading images!");
     new Image().src = '/images/loading_icon.svg';
-  });
-
-  const {user} = useContext(UserContext);
+  }, []);
 
   const upVoteArticle = () => {
     if (!user) {
       console.log("ERROR: Only logged in users can vote!");
 
+      setErrOverlayMsg(constants.ERR_MSG_NOT_LOGGED_IN);
       return;
     }
-
-
   };
 
   const downVoteArticle = () => {
     if(!user) {
       console.log("ERROR: Only logged in users can vote!");
 
+      setErrOverlayMsg(constants.ERR_MSG_NOT_LOGGED_IN);
       return;
     }
-
-
   }
 
   // TODO: Cleaner implementation of "/" and "/articles" routes.
@@ -60,6 +64,12 @@ function App() {
           <Route path="/register" element={<Register />} />
         </Routes>
       </main>
+
+      {errOverlayMsg ? 
+          <ErrorOverlay errOverlayMsg={errOverlayMsg} setErrOverlayMsg={setErrOverlayMsg}/> 
+        : 
+          null
+      }
     </>
   )
 }
