ca53aa511f69ff297c8db6255cf556b106b8bcd3
GANDHIX <mattdixon2000@hotmail.com>
2024-06-19 11:38:23 +0100

    Added users API endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
 const BASE_API_URL = `https://be-nc-news-4pai.onrender.com/api`;
+
 const ARTICLES_API_URL = `${BASE_API_URL}/articles`;
 const ARTICLE_BASE_API_URL = `${BASE_API_URL}/articles/`;
+const USERS_API_URL = `${BASE_API_URL}/users`;
+const USER_BASE_API_URL = `${BASE_API_URL}/users/`;
 
-export default {BASE_API_URL, ARTICLES_API_URL, ARTICLE_BASE_API_URL};
+export default {BASE_API_URL, ARTICLES_API_URL, ARTICLE_BASE_API_URL, USER_BASE_API_URL};
