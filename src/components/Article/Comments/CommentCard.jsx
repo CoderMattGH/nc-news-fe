662032e6c6306d45cf67f67abeaf1af7390ef7d7
GANDHIX <mattdixon2000@hotmail.com>
2024-06-20 14:11:40 +0100

    Added functionality to delete comment

--- src/components/Article/Comments/CommentCard.jsx
+++ src/components/Article/Comments/CommentCard.jsx
@@ -1,12 +1,51 @@
-import {useContext} from 'react';
-import dateParsing from '../../../util-functions/date-parsing';
+import axios from 'axios';
+import {useContext, useState} from 'react';
+
+import Loading from '../../Loading';
 
 import {UserContext} from '../../../contexts/User';
 
+import constants from '../../../constants';
+
+import dateParsing from '../../../util-functions/date-parsing';
+
 import './CommentCard.css';
 
-function CommentCard({comment}) {
+function CommentCard({comment, setComments, setErrOverlayMsg}) {
   const {user} = useContext(UserContext);
+  const [delBtnDisabled, setDelBtnDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDeleteCommentClick = (event) => {
+    event.preventDefault();
+
+    setDelBtnDisabled(true);
+    setIsLoading(true);
+
+    const url = `${constants.COMMENTS_API_URL}/${comment.comment_id}`;
+
+    axios.delete(url)
+        .then(() => {
+          console.log("Comment deleted successfully!");
+
+          // Remove comment from state
+          setComments((currCommentArr) => {
+            return currCommentArr.filter(
+                (com) => {
+                  return (com.comment_id !== comment.comment_id);
+                });
+          });
+        })
+        .catch((err) => {
+          console.log("Unable to delete comment!");
+          console.log(err);
+          setErrOverlayMsg("Unable to delete comment!");
+        })
+        .finally(() => {
+          setDelBtnDisabled(false);
+          setIsLoading(false);
+        });
+  };
 
   return (
     <div className="comment-card">
@@ -18,22 +57,30 @@ function CommentCard({comment}) {
           {dateParsing.convertUnixDate(comment.created_at)}
         </p>
       </div>
+
       <p className="comment-card__body">{comment.body}</p>
+
       <div className="comment-card-footer">
         <p className="comment-card__votes">
           <img className="button__vote_btn" src='/images/buttons/upvote.svg' />
           <span className="comment-card__vote_count">{comment.votes}</span>
           <img className="button__vote_btn" src='/images/buttons/downvote.svg' />
         </p>
-    
-        {(user.username === comment.author) ?
-          <button className="comment-delete-btn">
-            <img alt="Delete Comment "className="comment-icon" src="/images/buttons/delete.svg" />
+        {(user && (user.username === comment.author)) ?
+          <button className="comment-delete-btn" disabled={delBtnDisabled} 
+              onClick={handleDeleteCommentClick}>
+            <img alt="Delete Comment" className="comment-icon" src="/images/buttons/delete.svg" />
           </button>
             :
           null
         }
       </div>
+
+      {(isLoading) ?
+          <Loading size="tiny" />
+        :
+          null
+      }
     </div>
   );
 }
