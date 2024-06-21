657699d40dc69b13ae52fb19c0874acfddcf87c9
GANDHIX <mattdixon2000@hotmail.com>
2024-06-21 13:58:53 +0100

    Added initial search query functionality

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,9 +1,53 @@
+import DEBUG from '../../constants/debug';
+
+import {useState} from 'react';
+import {useSearchParams} from 'react-router-dom';
+
 import './SearchBar.css';
 
 function SearchBar() {
+  const MAX_SEARCH_LEN = 200;
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSearchInput = (event) => {
+    if (event.target.value.length > MAX_SEARCH_LEN)
+      return;
+
+    setSearchInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    if (DEBUG)
+      console.log("Submitting Search form!");
+
+    event.preventDefault();
+    
+    const searchString = searchInput.trim();
+
+    // Add search string to searchParams
+    setSearchParams((currSearchParams) => {
+      currSearchParams.set('search', searchString);
+
+      return currSearchParams;
+    });
+  };
+
+  const validateSearchString = (searchStr) => {
+    if (searchStr.trim() === "")
+      return {valid: false, msg:"Search cannot be empty!"};
+    
+    return {valid: true};
+  };
+
   return (
     <div className="search-bar">
-      <input className="search-bar__input" type="text" placeholder="Search NC News" />
+      <form onSubmit={handleSubmit}>
+        <input className="search-bar__input" onChange={handleSearchInput} value={searchInput} 
+            type="text" placeholder="Search NC News" />
+      </form>
     </div>
   );
 }
