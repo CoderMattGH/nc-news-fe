faddc47a010601dbe9c1491464fe8b273337e7cf
GANDHIX <mattdixon2000@hotmail.com>
2024-06-22 15:09:49 +0100

    Updated API location URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = `https://be-nc-news-4pai.onrender.com/api`;
+const BASE_API_URL = `https://nc-news-api.codermatt.com/api`;
 
 const ARTICLES_API_URL = `${BASE_API_URL}/articles`;
 const USERS_API_URL = `${BASE_API_URL}/users`;
