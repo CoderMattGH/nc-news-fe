41738acb1f969ae4feef1faa2f8b815b79da3e19
GANDHIX <mattdixon2000@hotmail.com>
2024-06-22 21:38:41 +0100

    Fixed searchbox not clearing text

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,29 @@
-import { Link } from 'react-router-dom';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
 import MenuBar from './MenuBar';
 import SearchBar from './SearchBar';
 
 import './index.css';
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleHomeClick = (event) => {
+    setSearchInput("");
+  };
+
   return (
     <header className="header">
       <div className="title">
-        <Link className="link" to="/">
+        <Link className="link" onClick={handleHomeClick} to="/">
           <img alt="Title Logo" className="title__logo" src="/images/logo.svg" />
         </Link>
-        <Link className="link" to="/">
+        <Link className="link" onClick={handleHomeClick} to="/">
           <img alt="Title Image" className="title__img" src="/images/title_main.svg" />
         </Link>
       </div>
-      <SearchBar />
+      <SearchBar setSearchInput={setSearchInput} searchInput={searchInput} />
       <MenuBar />
     </header>
   );
